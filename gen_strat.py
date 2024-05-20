4063748353a722f0794aac9a6dc9c84238c1db70
MIN <maksimivankov26@yandex.ru>
2024-05-20 05:43:41 +0300

    сет настроек работает

--- gen_strat.py
+++ gen_strat.py
@@ -3,12 +3,14 @@
 from imports import *
 
 
-class Strat_gen:
-    def __init__(self,settings):
-        
+class Gen_strat(object):
+    """docstring for ClassName."""
+    def __init__(self, settings):
+        super(Gen_strat, self).__init__()
+
         self.settings = settings
 
-        self.HOW_MACH_SET = '50' # размер сета нсатроек
+        self.HOW_MACH_SET = self.settings['HOW_MACH_SET'] # размер сета нсатроек
 
         if self.settings['regime_profit'] == 1: # если режим профита фиксированный
             self.TP = '0.005,0.008,0.01,0.012,0.014,0.016,0.018,0.02'
@@ -24,6 +26,11 @@ class Strat_gen:
             self.VALUE_TRADE_MAKS = '1.2,1.4,1.6,1.8,2' # объём динамический процент
         self.CANAL_MAX = '0.7,0.75,0.8,0.85,0.9' # нахождение внутри канала сверху
         self.CANAL_MIN = '0.1,0.2,0.3,0.4' # нахождение внутри канала снизу
+        self.LEVERAGE = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'
+
+        open(self.settings['set_strat'], "w").close() # очищаем файл перед новой записью
+        for i in range(self.HOW_MACH_SET):
+            self.generate_to_file()
 
 
 
@@ -36,12 +43,15 @@ class Strat_gen:
         self.VALUE_TRADE_MAKS_gen = random.choice(self.VALUE_TRADE_MAKS.split(','))
         self.TP_gen = random.choice(self.TP.split(','))
         self.SL_gen = random.choice(self.SL.split(','))
+        self.LEVERAGE_gen = random.choice(self.SL.split(','))
 
-        str_settings=f'{self.CANAL_MAX_gen},{self.CANAL_MIN_gen},{self.VALUE_TRADE_MIN_gen},{self.VALUE_TRADE_MAKS_gen},{self.TP_gen},{self.SL_gen}'
+        str_settings=f'{self.CANAL_MAX_gen},{self.CANAL_MIN_gen},{self.VALUE_TRADE_MIN_gen},{self.VALUE_TRADE_MAKS_gen},{self.TP_gen},{self.SL_gen},{self.LEVERAGE_gen}'
 
-        f = open(self.settings['set_strat_1'],'a',encoding='utf-8')
+        f = open(self.settings['set_strat'],'a',encoding='utf-8')
         f.write('\n'+str_settings)
         f.close()
+
+
 
 
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Сет формируется здесь же через режим - использовать готовый сет из файла, либо сформировать новый.
 
 from imports import *
+from gen_strat import Gen_strat
 
 class Main:
     # Срабатывает при создании объекта
@@ -12,38 +13,54 @@ class Main:
         
         self.settings = {}
 
-
         # Первичные данные, настройки системы
-        self.settings['MYDIR_TF'] = 'DF/big_data/'
-        self.settings['MYDIR_1min'] = 'DF/big_data/1min/'
-        self.settings['MYDIR_5min'] = 'DF/big_data/5min/'
-        self.settings['MYDIR_15min'] = 'DF/big_data/15min/'
-        self.settings['set_strat'] = 'set_settings/big_data/set_strat.txt'
+        self.settings['MYDIR_TF'] = 'big_data/'
+        self.settings['MYDIR_1min'] = 'big_data/1min/'
+        self.settings['MYDIR_5min'] = 'big_data/5min/'
+        self.settings['MYDIR_15min'] = 'big_data/15min/'
+        self.settings['set_strat'] = 'big_data/set_strat.txt'
         self.settings['how_mach_coin_trade'] = 20 # сколько монет торговать
-        self.settings['work_tf'] = '5min' # рабочий таймфрейм
-        self.settings['see_tf'] = '1min' # таймфрейм слежения
+        self.settings['HOW_MACH_SET'] = 20 # сколько настроек сгенерировать
+        self.settings['how_mach_coin_trade_start'] = 5 # c какой по счету монеты торговать из отсортированного списка
         self.settings['DEPOSIT'] = 100 # начальный депозит
-        self.settings['LEVERAGE'] = 1 # плечо
         self.settings['COMMISSION_MAKER'] = 0.002 # комиссия мейкер
         self.settings['COMMISSION_TAKER'] = 0.001 # комиссия тейкер
-        self.settings['VALUE_TRADE_MIN'] = 100000  # Объём минимальный в долларах
-        self.settings['VALUE_TRADE_MAKS'] = 1000000 # Объём максимальный в долларах - диапазон
-        self.settings['VALUE_TRADE_MIN_procent'] = 0.2 # процент объёма от дневного движения цены минимальный
-        self.settings['VALUE_TRADE_MAKS_procent'] = 1 # процент объёма от дневного движения цены максимальный
-        self.settings['TP'] = 0.012 # Тейк
-        self.settings['SL'] = 0.02 # Стоп
-        self.settings['TP_procent'] = 20 # Тейк в процентах
-        self.settings['strategy_coin'] = 1 # стратения выбора монет
 
+        self.settings['strategy_coin'] = 1 # стратения выбора монет
         self.settings['regime_set'] = 1 # 1 - использовать сет настреок из файла, 0 - сформировать сет настроек
         self.settings['regime_profit'] = 1 # Режим ТП и СЛ, 1 - фиксированный, 0 - Динамический
         self.settings['regime_volume'] = 1 # Режим объёмов, 1 - фиксированный, 0 - Динамический
 
+        # вспомогательные переменные
+        self.set_settings_once = []
+        self.count_settings = 0
+        self.set_settings = {} # здесь хранится сет настроек в виде словаря
+
+        self.get_set_settings() # получаем сет настроек в словарь 
 
 
-        if self.settings['regime_set'] == 0:
 
-        
+
+    # генерирует сет настроек в файл, если режим 0 или берет сет из файла при режиме 1, сохраняет данные в словарь set_settings 
+    def get_set_settings(self):
+        if self.settings['regime_set'] == 0: # если режим = 0 (сормировать сет натсроек)
+            Gen_strat(self.settings) # то формируем настройки в файл
+        with open(self.settings['set_strat'],encoding='utf-8') as file: # получаем данные из файла сета настроек
+            for line in file:
+               if line.rstrip()!='': # если строка не пустая
+                    self.set_settings_once = line.rstrip().split(',') # то добавляем настройки в массив
+                    self.set_settings[self.count_settings] = {}
+                    self.set_settings[self.count_settings]['CANAL_MAX'] = self.set_settings_once[0]
+                    self.set_settings[self.count_settings]['CANAL_MIN'] = self.set_settings_once[1]
+                    self.set_settings[self.count_settings]['VALUE_TRADE_MIN'] = self.set_settings_once[2]
+                    self.set_settings[self.count_settings]['VALUE_TRADE_MAKS'] = self.set_settings_once[3]
+                    self.set_settings[self.count_settings]['TP'] = self.set_settings_once[4]
+                    self.set_settings[self.count_settings]['SL'] = self.set_settings_once[5]
+                    self.set_settings[self.count_settings]['LEVERAGE'] = self.set_settings_once[6]
+
+                    self.count_settings+=1
+                   
+
 
 
 
